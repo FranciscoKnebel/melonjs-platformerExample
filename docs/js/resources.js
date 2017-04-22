14f138632299ab0f7becc074dd759d9ef6861d0f
Francisco Knebel <franciscopaivaknebel@gmail.com>
2017-04-22 17:18:03 -0300

    Added docs folder for GitHub Pages.

---
+++ docs/js/resources.js
@@ -0,0 +1,92 @@
+game.resources = [
+    {
+        "name": "dst-inertexponent",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "cling",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "die",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "jump",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "lost",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "stomp",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "area01_bkg0",
+        "type": "image",
+        "src": "data/img/area01_bkg0.png"
+    },
+    {
+        "name": "area01_bkg1",
+        "type": "image",
+        "src": "data/img/area01_bkg1.png"
+    },
+    {
+        "name": "title_screen",
+        "type": "image",
+        "src": "data/img/gui/title_screen.png"
+    },
+    {
+        "name": "area01_level_tiles",
+        "type": "image",
+        "src": "data/img/map/area01_level_tiles.png"
+    },
+    {
+        "name": "gripe_run_right",
+        "type": "image",
+        "src": "data/img/sprite/gripe_run_right.png"
+    },
+    {
+        "name": "spinning_coin_gold",
+        "type": "image",
+        "src": "data/img/sprite/spinning_coin_gold.png"
+    },
+    {
+        "name": "wheelie_right",
+        "type": "image",
+        "src": "data/img/sprite/wheelie_right.png"
+    },
+    {
+        "name": "map1",
+        "type": "tmx",
+        "src": "data/map/map1.tmx"
+    },
+    {
+        "name": "map2",
+        "type": "tmx",
+        "src": "data/map/map2.tmx"
+    },
+    {
+        "name": "map01_level_tileset",
+        "type": "tsx",
+        "src": "data/map/map01_level_tileset.tsx"
+    },
+    {
+        "name": "PressStart2P",
+        "type": "image",
+        "src": "data/fnt/PressStart2P.png"
+    },
+    {
+        "name": "PressStart2P",
+        "type": "binary",
+        "src": "data/fnt/PressStart2P.fnt"
+    }
+];
